e6b1bfcf999c163433dc0c43b3f65a19d1fc6f96
Evelin <eveyazmins@gmail.com>
2020-10-20 00:39:53 -0300

    listados

---
+++ src/reducers/usuariosReducer.js
@@ -0,0 +1,14 @@
+import { types } from '../types/types';
+
+const initialState = {
+    usuarios: [{
+        name: "Eve",
+        nombre: "Evelin",
+        apellido: "Santamaria",
+        dni: 38615448,
+        //password: '#fafafa',
+        perfil: "admin",
+    }],
+    
+};
+
